91d417c3f219ee7c7d2dfff19910adda5054bafc
George Higbie <gehigbie@gmail.com>
2017-01-13 20:46:00 -0700

    added functions and conditional statement

--- js.js
+++ js.js
@@ -1,3 +1,31 @@
 //this file was created in the terminal
+var actions = [];
+
 var question = "What would you like to do?";
 var choice = prompt(question);
+
+
+if(input === "list"){
+	list();
+}
+else if(input === "new"){
+	newItem();
+}
+else if(input === "quit"){
+	//do nothing
+}
+else{
+
+}
+
+function newItem(){
+	var item = prompt("What should be added to the list?");
+	actions.push(item);
+	console.log(actions);
+}
+
+function list(){
+	console.log(actions);
+}
+
+
