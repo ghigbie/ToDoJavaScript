8ab53dc95df97eacc8657adbdc6138b05ae24d65
George Higbie <gehigbie@gmail.com>
2017-01-14 18:02:46 -0700

    modified delete part of method

--- js.js
+++ js.js
@@ -16,11 +16,13 @@ while(input !== "quit"){
 		var newItem = prompt("Enter a new item to add to the list.");
 		newItem = newItem.charAt(0).toUpperCase() + newItem.substr(1);
 		actions.push(newItem);
-		console.log(`${item} added to the list`);
+		console.log(`${newItem} added to the list.`);
 	}
-	else if(choice === "delete"){
-		var popedItem = actions.pop();
-		console.log(`${popedItem} removed from the list.`)
+	else if(input === "delete"){
+		var index = prompt("Enter idex of item to delete.")
+		var itemDeleted = actions(index);
+		actions.splice(index, 1); //splice will remove the items from the list following the index
+		console.log(`${itemDeleted} removed from the list.`);
 	}
 	else{
 		alert("Please enter a valid action!")
@@ -31,33 +33,33 @@ while(input !== "quit"){
 alert("Ok, its time to go!");
 
 
-runApp();
+// runApp();
 
-function runApp(){
-	var item;
-	while(choice !== "quit"){
-		if(choice === "list"){
-			console.log("**************");
-			actions.forEach(function(item, index){
-				console.log(`${index}: ${item}`);
-			});
-			console.log("**************");
-		}
-		else if(choice === "new"){
-			item = prompt("What should be added to the list?");
-			actions.push(item);
-			console.log(`${item} added to the list. Thank you!`);
-		}
-		else if(choice === "delete"){
-			var popedItem = actions.pop();
-			console.log(`${popedItem} removed from the list.`)
-		}
-		else{
-			alert("Please enter a valid action!");
-		}
-		choice = prompt(question);
-		runApp();
-	}
-}
+// function runApp(){
+// 	var item;
+// 	while(choice !== "quit"){
+// 		if(choice === "list"){
+// 			console.log("**************");
+// 			actions.forEach(function(item, index){
+// 				console.log(`${index}: ${item}`);
+// 			});
+// 			console.log("**************");
+// 		}
+// 		else if(choice === "new"){
+// 			item = prompt("What should be added to the list?");
+// 			actions.push(item);
+// 			console.log(`${item} added to the list. Thank you!`);
+// 		}
+// 		else if(choice === "delete"){
+// 			var popedItem = actions.pop();
+// 			console.log(`${popedItem} removed from the list.`)
+// 		}
+// 		else{
+// 			alert("Please enter a valid action!");
+// 		}
+// 		choice = prompt(question);
+// 		runApp();
+// 	}
+// }
 
-alert("Ok, its time to go!");
+// alert("Ok, its time to go!");
